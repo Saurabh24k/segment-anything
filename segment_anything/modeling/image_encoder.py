07287aaca495123c9423bc5f745022c79b9f4441
Saurabh Rajput <50542343+Saurabh24k@users.noreply.github.com>
2024-11-13 18:58:09 -0600

    Update image_encoder.py

--- segment_anything/modeling/image_encoder.py
+++ segment_anything/modeling/image_encoder.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import Optional, Tuple, Type
+from segment_anything.modeling.transformer import Block
+
 
 class ImageEncoderViT(nn.Module):
     def __init__(
